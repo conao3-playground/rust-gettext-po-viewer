6a5996a0025dcd7cb9019e4d0fe7f065e0318d40
Naoya Yamashita <conao3@gmail.com>
2024-01-28 02:36:33 +0900

    add c_po_file_read sample

--- build.rs
+++ build.rs
@@ -1,5 +1,8 @@
 fn main() {
     cc::Build::new()
         .file("src/gettext.c")
+        .flag("-lgettextpo")
         .compile("gettext");
+
+    println!("cargo:rustc-link-lib=gettextpo");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
-extern crate libc;
+use std::ffi::CString;
 
-extern {
+extern "C" {
     fn double_input(input: libc::c_int) -> libc::c_int;
+    fn c_po_file_read(filename: *const libc::c_char) -> *mut libc::c_void;
 }
 
 fn main() {
@@ -10,4 +11,11 @@ fn main() {
     let input = 4;
     let output = unsafe { double_input(input) };
     println!("{} * 2 = {}", input, output);
+
+    let args: Vec<String> = std::env::args().collect();
+    println!("{:?}", args);
+
+    let filename = CString::new(args[1].as_str()).unwrap();
+    let file = unsafe { c_po_file_read(filename.as_ptr()) };
+    println!("{:?}", file);
 }
